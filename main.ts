d3ac6cada8122f6299fe9e21c9bfc37a5b3beb1a
Jan <154762684+jbuck95@users.noreply.github.com>
2025-03-10 18:20:13 +0100

    2

--- main.ts
+++ main.ts
@@ -1,85 +1,107 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EditorView, ViewUpdate } from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface SlashCommandsPluginSettings {
+	codeBlockLanguage: string;
+	latexCommands: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SlashCommandsPluginSettings = {
+	codeBlockLanguage: 'javascript',
+	latexCommands: [
+		'\\newline',
+		'\\pagebreak',
+		'\\textbf{}',
+		'\\textit{}',
+		'\\underline{}',
+		'\\begin{equation}\\end{equation}',
+		'\\begin{align}\\end{align}',
+		'\\frac{}{}',
+		'\\sum_{}^{}',
+		'\\int_{}^{}'
+	]
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SlashCommandsPlugin extends Plugin {
+	settings: SlashCommandsPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Register the slash command handler
+		this.registerEditorExtension([
+			this.createSlashCommandExtension()
+		]);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add settings tab
+		this.addSettingTab(new SlashCommandsSettingTab(this.app, this));
 	}
-
-	onunload() {
-
+	createSlashCommandExtension() {
+		// This is a simplified implementation using proper CodeMirror extension
+		return EditorView.updateListener.of((update: ViewUpdate) => {
+			if (!update.docChanged) return;
+			
+			const changes = update.changes;
+			const editor = this.app.workspace.activeEditor?.editor;
+			if (!editor) return;
+			handleKey: (key: string, editor: Editor) => {
+				// Check if the key is a space and the last word is a slash command
+				if (key === ' ') {
+					const cursor = editor.getCursor();
+					const line = editor.getLine(cursor.line);
+					const beforeCursor = line.substring(0, cursor.ch - 1);
+					
+					if (beforeCursor.endsWith('/code')) {
+						// Replace "/code " with a code block
+						const codeBlock = '```' + this.settings.codeBlockLanguage + '\n\n```';
+						editor.replaceRange(
+							codeBlock, 
+							{ line: cursor.line, ch: cursor.ch - 6 }, // 6 chars = "/code "
+							{ line: cursor.line, ch: cursor.ch }
+						);
+						
+						// Position cursor inside the code block
+						editor.setCursor({ 
+							line: cursor.line + 1, 
+							ch: 0 
+						});
+						
+						return true; // Handled
+					}
+					
+					if (beforeCursor.endsWith('/latex')) {
+						// Show the LaTeX command suggestions
+						this.showLatexCommandSuggestions(editor, cursor);
+						return true; // Handled
+					}
+				}
+				
+				return false; // Not handled
+			}
+		});
+	}
+	
+	showLatexCommandSuggestions(editor: Editor, cursor: any) {
+		// Create and show modal with LaTeX commands
+		const modal = new LatexCommandsModal(this.app, this.settings.latexCommands, (command: string) => {
+			// Replace "/latex " with the selected LaTeX command
+			editor.replaceRange(
+				command, 
+				{ line: cursor.line, ch: cursor.ch - 7 }, // 7 chars = "/latex "
+				{ line: cursor.line, ch: cursor.ch }
+			);
+			
+			// If the command has braces, position cursor inside first brace
+			const cursorOffset = command.indexOf('{}');
+			if (cursorOffset !== -1) {
+				editor.setCursor({ 
+					line: cursor.line, 
+					ch: cursor.ch - 7 + cursorOffset + 1 
+				});
+			}
+		});
+		
+		modal.open();
 	}
 
 	async loadSettings() {
@@ -91,44 +113,100 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class LatexCommandsModal extends Modal {
+	commands: string[];
+	onChoose: (command: string) => void;
+
+	constructor(app: App, commands: string[], onChoose: (command: string) => void) {
 		super(app);
+		this.commands = commands;
+		this.onChoose = onChoose;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.empty();
+		
+		contentEl.createEl('h2', { text: 'LaTeX Commands' });
+		
+		const commandsContainer = contentEl.createDiv('latex-commands-container');
+		
+		for (const command of this.commands) {
+			const commandEl = commandsContainer.createDiv('latex-command-item');
+			commandEl.setText(command);
+			commandEl.addEventListener('click', () => {
+				this.onChoose(command);
+				this.close();
+			});
+		}
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SlashCommandsSettingTab extends PluginSettingTab {
+	plugin: SlashCommandsPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SlashCommandsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Slash Commands Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Default Code Block Language')
+			.setDesc('The programming language to use for code blocks by default')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.codeBlockLanguage)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.codeBlockLanguage = value;
 					await this.plugin.saveSettings();
+				}));
+		
+		containerEl.createEl('h3', { text: 'LaTeX Commands' });
+		containerEl.createEl('p', { text: 'Add or remove LaTeX commands that appear in the suggestion list' });
+		
+		const latexCommandsContainer = containerEl.createDiv('latex-commands-settings');
+		
+		// Add existing commands
+		this.plugin.settings.latexCommands.forEach((command, index) => {
+			this.addLatexCommandSetting(latexCommandsContainer, command, index);
+		});
+		
+		// Add button to add new command
+		new Setting(containerEl)
+			.addButton(button => button
+				.setButtonText('Add LaTeX Command')
+				.onClick(() => {
+					this.plugin.settings.latexCommands.push('\\newcommand');
+					this.plugin.saveSettings();
+					this.display(); // Refresh the settings tab
+				}));
+	}
+	
+	addLatexCommandSetting(containerEl: HTMLElement, command: string, index: number): void {
+		new Setting(containerEl)
+			.addText(text => text
+				.setValue(command)
+				.onChange(async (value) => {
+					this.plugin.settings.latexCommands[index] = value;
+					await this.plugin.saveSettings();
+				}))
+			.addButton(button => button
+				.setButtonText('Remove')
+				.onClick(async () => {
+					this.plugin.settings.latexCommands.splice(index, 1);
+					await this.plugin.saveSettings();
+					this.display(); // Refresh the settings tab
 				}));
 	}
 }
